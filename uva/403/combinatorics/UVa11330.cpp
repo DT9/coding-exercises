3253490a45a80193bb9dc7a84e35cd11611b0464
Dennis Truong <dtruong1@ualberta.ca>
2017-02-27 15:55:16 -0700

    andys shoes

--- uva/403/combinatorics/UVa11330.cpp
+++ uva/403/combinatorics/UVa11330.cpp
@@ -6,7 +6,7 @@
  * 
  * Brief problem description: 
  *
- *   
+ *   given an array of shoe pairs, find min # of swaps
  *
  * Solution Summary:
  *
@@ -14,7 +14,7 @@
  *
  * Used Resources:
  *
- *   
+ *   cp3
  *
  * I hereby certify that I have produced the following solution myself 
  * using the resources listed above in accordance with the CMPUT 403 
@@ -24,26 +24,35 @@
  */
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
-vector<int> v;
-int tc,n,s;
+int tc, n, s, l, v[10005];
 
-int swap() {
-    
-    return 0;
-}
-
-int main() {
+int main()
+{
     cin >> tc;
-    while (tc--) {
+    while (tc--)
+    {
         cin >> n;
-        for (int i = 0; i < 2*n; i++){
-            cin >> s;
-            v.push_back(s);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> s >> l;
+            v[s] = l;
         }
-        cout << swap() << endl;
-        v.clear();
+        int count = 0;
+        for (int i = 1, c = v[i]; i <= 10000; i++, c = v[i]) {
+            if (v[i] != 0) {
+                v[i] = 0;
+                while (v[c] != 0) {
+                    count++;
+                    int tmp = v[c];
+                    v[c] = 0;
+                    c = tmp;
+                }
+            }
+        }
+        cout << count << endl;
     }
     return 0;
 }
