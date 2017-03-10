565425a8932f27f39af1a84da58a93aaf37e99a0
Dennis Truong <dtruong1@ualberta.ca>
2017-03-10 16:39:27 -0700

    brute force n queens

--- uva/403/backtracking/UVa11195.cpp
+++ uva/403/backtracking/UVa11195.cpp
@@ -22,3 +22,41 @@
  *
  * --- Dennis Truong
  */
+/* 8 Queens Chess Problem */
+#include <cstdlib>                       // we use the int version of 'abs'
+#include <cstdio>
+#include <cstring>
+#include <bitset>
+#include <iostream>
+using namespace std;
+
+// ok to use global variables
+int row[8], TC, a, b, lineCounter, n, ans; 
+char board[15][15];
+bitset<30> rw, ld, rd; // for the largest n = 14, we have 27 diagonals
+bool place(int r, int c) {
+  for (int prev = 0; prev < c; prev++)    // check previously placed queens
+    if (row[prev] == r || (abs(row[prev] - r) == abs(prev - c)))
+      return false;        // share same row or same diagonal -> infeasible
+  return true; }
+
+void backtrack(int c) {
+    if (c == n) { ans++; return; } // a solution 
+    for (int r = 0; r < n; r++) // try all possible row
+    if (board[r][c] != '*' && !rw[r] && !ld[r - c + n - 1] && !rd[r + c]) { 
+        rw[r] = ld[r - c + n - 1] = rd[r + c] = true; // flag off 
+        backtrack(c + 1);
+        rw[r] = ld[r - c + n - 1] = rd[r + c] = false; // restore
+}   }
+int main() {
+int tc = 1;
+string s;
+  while (cin >> n,n) {
+    memset(board,'.',sizeof(board));
+    for (int i = 0; i < n; i++){
+        cin >> board[i];
+    }
+    backtrack(0);    // generate all possible 8! candidate solutions
+    cout << "Case " << tc << ": " << ans << endl;
+    tc++; ans = 0;
+} } // return 0;
